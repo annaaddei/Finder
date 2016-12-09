80dc973319a661631920027af466c0a00cdc7335
Anna Addei <anna.addei@gmail.com>
2016-12-09 01:20:04 +0000

    phonebook

--- js/my_script.js
+++ js/my_script.js
@@ -77,6 +77,14 @@ function makeReservation(name){
     window.location.assign("reservation.html?restaurant="+name);
 }
 
+document.getElementById("inviteButton").onclick = function() {
+    navigator.contacts.pickContact(function(contact){
+        console.log('The following contact has been selected:' + JSON.stringify(contact));
+    },function(err){
+        console.log('Error: ' + err);
+    });
+}
+
 
 
 $(function () {
